31537648b5e0879a9078e8babf3e27d4b71d36df
me6edi <exited17@gmail.com>
2025-07-15 15:10:43 +0600

    categoryshowcase

--- react-ecommerce-website/src/home/HomeCategory.jsx
+++ react-ecommerce-website/src/home/HomeCategory.jsx
@@ -46,40 +46,42 @@ const categoryList = [
 ];
 
 export const HomeCategory = () => {
-  return (
-    <div className="category-section style-4 padding-tb">
-      <div className="container">
-        {/* section */}
-        <div className="section-header text-center">
-          <span className="subtitle">{subTitle}</span>
+  return <div className="category-section style-4 padding-tb">
+    <div className="container">
+      {/* section header */}
+      <div className="section-header text-center">
+          <span>{subTitle}</span>
           <h2 className="title">{title}</h2>
-        </div>
+      </div>
 
-
-        <div className="section-wrapper">
+      {/* section-card */}
+      <div className="section-wrapper">
           <div className="row g-4 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1">
-            {categoryList.map((val, i) => (
-              <div key={i} className="col">
-                <Link to={"/shop"} className="category-item">
+            {
+              categoryList.map((val,i) => (<div key={i} className="col">
+                <Link to="/shop" className="category-item">
                   <div className="category-inner">
-                    {/* image thumbnail */}
-                    <div>
+                    {/* imge thumbnail */}
+                    <div className="category-thumb">
                       <img src={val.imgUrl} alt="" />
                     </div>
+
                     {/* content */}
                     <div className="category-content">
-                        <div className="cate-icon">
-                            <i className={`${val.iconName}`}></i>
-                        </div>
-                        <Link to="/shop" className="lab-btn"><h6>{val.title}</h6></Link>
+                      <div className="cate-icon">
+                        <i className={val.iconName}></i>
+                      </div>
+                      <Link to="/shop"><h6>{val.title}</h6></Link>
                     </div>
                   </div>
                 </Link>
-              </div>
-            ))} 
-          </div>
-        </div>
+              </div>))
+            }
+            </div>
+            <div className="text-center mt-5">
+              <Link to="/shop" className="lab-btn"><span>{btnText}</span></Link>
+            </div>
       </div>
     </div>
-  );
+  </div>
 };
